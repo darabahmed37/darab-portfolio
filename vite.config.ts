77d83f6374520586b44810426db5b1845a22452e
Darab Ahmed <darabahmed37@gmail.com>
2025-05-18 15:56:38 +0500

    update index.html and vite.config.ts; change title to 'Darab's Portfolio' and set base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,28 @@
-import {defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import fs from 'fs';
-import path, {dirname} from 'path';
-import {fileURLToPath} from 'url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const srcPath = path.resolve(__dirname, 'src');
+const srcPath = path.resolve(__dirname, "src");
 
 // Dynamically create aliases like "@components", "@assets", etc.
-const folderAliases = fs.readdirSync(srcPath, {withFileTypes: true})
-    .filter(dirent => dirent.isDirectory())
-    .reduce((aliases, dirent) => {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        aliases[`@${dirent.name}`] = path.resolve(srcPath, dirent.name);
-        return aliases;
-    }, {});
+const folderAliases = fs
+  .readdirSync(srcPath, { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .reduce((aliases, dirent) => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    aliases[`@${dirent.name}`] = path.resolve(srcPath, dirent.name);
+    return aliases;
+  }, {});
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: folderAliases,
-    },
+  plugins: [react(), tailwindcss()],
+  base: "/darab-portfolio/",
+  resolve: {
+    alias: folderAliases,
+  },
 });
